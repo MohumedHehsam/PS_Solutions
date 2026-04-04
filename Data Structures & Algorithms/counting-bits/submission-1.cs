24c72d4d844bbd94af4351c74a88762b7bff96c1
MohumedHehsam <mohumedheshamtaha@gmail.com>
2026-04-04 16:34:59 +0200

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] CountBits(int n) 
+    {
+        int[] res = new int[n+1];
+        
+        for(int i=1;i<=n;i++)
+        {
+            res[i]=i%2+res[i>>1];
+        }
+        return res;
+    }
+}
