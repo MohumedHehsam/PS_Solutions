33aacb98688ba9c66c4bf2f231becc0b8aeb9308
MohumedHehsam <mohumedheshamtaha@gmail.com>
2026-04-04 16:41:35 +0200

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] CountBits(int n) 
+    {
+        int[] res = new int[n+1];
+        
+        for(int i=1;i<=n;i++)
+        {
+            res[i]=res[i&(i-1)]+1; // removing rightmost bit
+        }
+        return res;
+    }
+}
